1699ab284e1032f3ab0c1447e67606d33f69f0ae
Benchaphol07 <68273733+Benchaphol07@users.noreply.github.com>
2020-08-10 23:22:22 +0700

    Add files via upload

---
+++ Arisa.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int score(int *midterm,int *final,int *hw);
+int show(int *midterm,int *final,int *hw,int *sum);
+int cal(int score);
+int main()
+{
+	int midterm,final,hw,sum;
+	score(&midterm,&final,&hw);
+	sum=midterm+final+hw;
+	show(&midterm,&final,&hw,&sum);
+	return 0;
+}
+int score(int *midterm,int *final,int *hw)
+{
+	cout << "Enter your midterm score : ";
+	cin >> *midterm;
+	cout << "Enter your final score : ";
+	cin >> *final;
+	cout << "Enter your homework score : ";
+	cin >> *hw;
+	return *midterm + *final + *hw;     // <---- i don't know how tf this work lol   so don't ask gu  ok
+}
+int cal(int score)
+{
+	//int score;
+	string gpa;
+	//cout << "Enter your score : ";
+	//cin >> score;
+	//**********************************************************
+	if (score > 100 || score < 0 )
+	{
+		cout << "Your Score Inccorect " << endl;
+	exit(0);
+	}
+	//************************************************************
+	if (score >= 80 && score <= 100)  gpa =" A ";
+	if (score >= 75 && score <= 79)  gpa = " B+" ;
+	if (score >= 70 && score <= 74 )  gpa =  " B ";
+	if (score >= 65 && score <= 69 )  gpa =  " C+";
+	if (score >= 60 && score <= 64)  gpa =  " C " ;
+	if (score >= 55 && score <= 59 )  gpa =  " D+";
+	if (score >= 50 && score <= 54 ) gpa = " D ";
+	if (score < 50 )  gpa = " F " ;
+	//************************************************************
+	cout << "Your score is : " << score << endl;
+	cout << "Grade : " << gpa << endl;
+	return(0);
+}
+int show(int *midterm,int *final,int *hw,int *sum)
+{
+	cout << "Midterm score : " << *midterm << endl ;
+	cout << "Final score : "<< *final << endl;
+	cout << "Homework score : " << *hw << endl;
+	cal(*sum);
+	//cout << "Grade : "<< gpa;
+	cout << endl;
+	return 0;                       
+}
