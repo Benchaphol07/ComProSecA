39da02719fab3bc934595f605c2140b9c136e454
Benchaphol07 <68273733+Benchaphol07@users.noreply.github.com>
2020-08-11 21:11:31 +0700

    Add files via upload

---
+++ loopage.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int calAge(int);
+int main ()
+{
+	int year;
+	for (int n=1; n<=3; n++ )
+	{
+		cout << "Enter Year " << n << " : ";
+		cin >> year;
+		int age = calAge(year) ;
+		cout << "Age " << n << " : " << age << endl;
+	}
+	cout << "\nThank you." << endl;
+	return (0);
+}
+int calAge(int year)
+{
+	return(2563 - year);
+}
