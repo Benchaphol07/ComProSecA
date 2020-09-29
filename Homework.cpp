36a29361682eb5bb2375a5c26526f5b3116d76b4
Benchaphol07 <68273733+Benchaphol07@users.noreply.github.com>
2020-09-29 12:23:56 +0700

    Add files via upload

--- Homework.cpp
+++ Homework.cpp
@@ -1,40 +1,35 @@
-#include <iostream>
-#include <string>
+#include<iostream>
 using namespace std;
+void display(int salary[], int);
+int cal_bonus(int);
 int main()
 {
-	float first, second, third, midterm, final, total, sum ;
-	string result;
-	
-		cout << "==========QUIZZES===============" << endl;
-		cout << "Enter first quizz (10) : ";
-		cin >> first;
-		cout << "Enter second quizz (10) : ";
-		cin >> second;
-		cout << "Enter third quizz (10) : ";
-		cin >> third;
-		
-		cout << "==========MID-TERM==============" << endl;
-		cout << "Enter mid-term (40) : ";
-		cin >> midterm;
-
-		cout << "===========FINAL================" << endl;
-		cout << "Enter final (50) : ";
-		cin >> final;
-
-		//Sum of quizzes
-		total = (first + second + third) / 3;
-		cout << "Quizzes Total : " << total << endl;
-		cout << "Mid term : " << midterm << endl;
-		cout << "Final : " << final << endl;
-
-		//Total
-		sum = midterm + final + total;
-		cout << "Total : " << sum << endl;
-
-	
-		result = (sum >= 50) ? "You score is PASS" : "You score is FAIL";
-		cout << result << endl;
-	
-	return 0;
+	int num;
+	cout << "Enter Number of person : ";
+	cin >> num;
+	cout << endl;
+	int *salary = new int[num];
+	for(int i = 0; i<num; i++)
+	{
+		cout << "Enter the salary [" << i+1 << "] : ";
+		cin >> salary[i];
+		cout << endl;
+	}
+	display(salary, num);
+	return(0);
+}
+void display(int salary[], int num)
+{
+	cout << "---------------------------------------------------------\n";
+	cout << "There are " << num << " persons." << endl;
+	for(int i = 0; i < num; i++)
+	{
+		cout << "The Salary for person ["<< i+1 << "] = " << salary[i];
+		cout << " and Bonus = " << cal_bonus(salary[i]) << endl;
+	}
+	cout << "---------------------------------------------------------\n";
+}
+int cal_bonus(int salary)
+{
+	return(salary*2);
 }
